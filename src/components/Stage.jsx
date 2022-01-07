e25ae4a74b999c617d847291baa84bc0355df80e
nocdim <nokkudimu@gmail.com>
2022-01-08 02:28:37 +0300

    few comments added

--- src/components/Stage.jsx
+++ src/components/Stage.jsx
@@ -4,6 +4,7 @@ import Cell from "./Cell"
 const Stage = ({ stage }) => {
     return (
         <div>
+            {/* for each cell rendering Cell component (mapping through stage (gameHelpers) and through rows) */}
             {stage.map(row => row.map((cell, x) => <Cell key={x} type={cell[0]} />))}
         </div>
     )
--- src/gameHelpers.js
+++ src/gameHelpers.js
@@ -2,6 +2,7 @@ export const STAGE_WIDTH = 12
 export const STAGE_HEIGHT = 20
 
 // idk why the syntax is like this (without {})
+// grid creation 
 export const createStage = () =>
     Array.from(Array(STAGE_HEIGHT), () =>
         new Array(STAGE_WIDTH).fill([0, 'clear'])
