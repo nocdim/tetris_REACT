947c045bd674a0a529394e27bbafbbfb1b1fe20b
nocdim <nokkudimu@gmail.com>
2022-01-08 18:40:08 +0300

    StyledStartButton created

--- src/components/StartButton.jsx
+++ src/components/StartButton.jsx
@@ -1,10 +1,11 @@
 import React from "react";
+import { StyledStartButton } from "./styles/StyledStartButton";
 
 const StartButton = ({ callback }) => {
     return (
-        <div>
+        <StyledStartButton onClick={callback}>
             Start Game
-        </div>
+        </StyledStartButton>
     )
 }
 
